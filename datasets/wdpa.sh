10511ef8989d322c7c36925b855a87802ea958d6
Guillaume Larocque <glaroc@gmail.com>
2025-12-12 13:47:50 -0500

    test

--- datasets/wdpa.sh
+++ datasets/wdpa.sh
@@ -4,7 +4,7 @@
 # Convert the GDB to Parquet format using ogr2ogr
 #wget -P /data/ https://d1gam3xoknrgr2.cloudfront.net/current/WDPA_Dec2025_Public.zip
 #unzip /data/WDPA_Dec2025_Public.zip -d /data/wdpa_dec2025_public   
-ogr2ogr -f Parquet /data/wdpa.gpkg /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb wdpa_polygons -lco GEOMETRY=AS_WKB -nlt PROMOTE_TO_MULTI -nln wdpa
+ogr2ogr -f Parquet /data/wdpa.gpkg /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb ogr2ogr -f Parquet /data/wdpa.gpkg /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb WDPA_Dec2025_Public -lco GEOMETRY=AS_WKB -nlt PROMOTE_TO_MULTI -nln wdpa -lco GEOMETRY=AS_WKB -nlt PROMOTE_TO_MULTI -nln wdpa
 /root/.duckdb/cli/latest/duckdb < /datasets/wdpa.sql
 s5cmd cp --acl "public-read" /data/wdpa.parquet s3://bq-io/vectors-cloud/wdpa/
 
