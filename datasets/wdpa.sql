a0a02a1d161ed6d4f9203990cba70a8ae4bdc6fd
Guillaume Larocque <glaroc@gmail.com>
2025-12-12 12:22:27 -0500

    Remove SHAPE

--- datasets/wdpa.sql
+++ datasets/wdpa.sql
@@ -3,4 +3,4 @@ INSTALL httpfs;
 LOAD spatial;
 LOAD httpfs;
 CREATE VIEW wdpa AS SELECT * FROM ST_READ('/data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb');
-COPY (SELECT wdpa.*, SHAPE as geometry, ST_XMIN(SHAPE) as minx, ST_YMIN(SHAPE) as miny, ST_XMAX(SHAPE) as maxx, ST_YMAX(SHAPE) as maxy FROM wdpa ORDER BY ST_Hilbert(SHAPE, ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90)))) TO '/data/wda.parquet' (FORMAT PARQUET, COMPRESSION zstd);
+COPY (SELECT wdpa.*, SHAPE as geometry, ST_XMIN(SHAPE) as minx, ST_YMIN(SHAPE) as miny, ST_XMAX(SHAPE) as maxx, ST_YMAX(SHAPE) as maxy EXCLUDE (SHAPE) FROM wdpa ORDER BY ST_Hilbert(SHAPE, ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90)))) TO '/data/wdpa.parquet' (FORMAT PARQUET, COMPRESSION zstd);
