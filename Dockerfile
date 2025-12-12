0215835cd1b356ef56bb1d8f0638f44c1ae768c6
Guillaume Larocque <glaroc@gmail.com>
2025-12-12 11:42:50 -0500

    WIP wdpa

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM ghcr.io/osgeo/gdal:ubuntu-full-3.12.0 as gdal
 WORKDIR /app
 
 RUN apt-get update && apt-get install -y wget
+RUN curl https://install.duckdb.org | sh
 RUN wget https://github.com/peak/s5cmd/releases/download/v2.3.0/s5cmd_2.3.0_linux_amd64.deb
 
 RUN dpkg -i s5cmd_2.3.0_linux_amd64.deb
---
+++ datasets/wdpa.sql
@@ -0,0 +1,8 @@
+SET threads = 2;
+SET preserve_insertion_order=false;
+INSTALL spatial;
+INSTALL httpfs;
+LOAD spatial;
+LOAD httpfs;
+CREATE VIEW wdpa AS SELECT * FROM ST_READ('/data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb');
+COPY (SELECT wdpa.*, SHAPE as geometry, ST_XMIN(SHAPE) as minx, ST_YMIN(SHAPE) as miny, ST_XMAX(SHAPE) as maxx, ST_YMAX(SHAPE) as maxy FROM wdpa ORDER BY ST_Hilbert(SHAPE, ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90)))) TO '/data/wda.parquet' (FORMAT PARQUET, COMPRESSION zstd);
