3d4b461be17651fd0be12682c31a2a4f95b30d57
Guillaume Larocque <glaroc@gmail.com>
2025-12-12 13:46:43 -0500

    test

--- datasets/wdpa.sh
+++ datasets/wdpa.sh
@@ -2,9 +2,9 @@
 
 # FROM https://www.protectedplanet.net/en/thematic-areas/wdpa?tab=WDPA
 # Convert the GDB to Parquet format using ogr2ogr
-wget -P /data/ https://d1gam3xoknrgr2.cloudfront.net/current/WDPA_Dec2025_Public.zip
-unzip /data/WDPA_Dec2025_Public.zip -d /data/wdpa_dec2025_public   
-#ogr2ogr -f Parquet /data/wdpa.parquet /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb WDPA_poly_Dec2025
+#wget -P /data/ https://d1gam3xoknrgr2.cloudfront.net/current/WDPA_Dec2025_Public.zip
+#unzip /data/WDPA_Dec2025_Public.zip -d /data/wdpa_dec2025_public   
+ogr2ogr -f Parquet /data/wdpa.gpkg /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb wdpa_polygons -lco GEOMETRY=AS_WKB -nlt PROMOTE_TO_MULTI -nln wdpa
 /root/.duckdb/cli/latest/duckdb < /datasets/wdpa.sql
 s5cmd cp --acl "public-read" /data/wdpa.parquet s3://bq-io/vectors-cloud/wdpa/
 
--- datasets/wdpa.sql
+++ datasets/wdpa.sql
@@ -2,5 +2,5 @@ INSTALL spatial;
 INSTALL httpfs;
 LOAD spatial;
 LOAD httpfs;
-CREATE VIEW wdpa AS SELECT * FROM ST_READ('/data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb');
-COPY (SELECT wdpa.*, REPLACE(SHAPE as geometry), ST_XMIN(SHAPE) as minx, ST_YMIN(SHAPE) as miny, ST_XMAX(SHAPE) as maxx, ST_YMAX(SHAPE) as maxy FROM wdpa ORDER BY ST_Hilbert(SHAPE, ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90)))) TO '/data/wdpa.parquet' (FORMAT PARQUET, COMPRESSION zstd);
+CREATE VIEW wdpa AS SELECT * FROM ST_READ('/data/wdpa.gpkg', 'wdpa_polygons');
+COPY (SELECT wdpa.* REPLACE(SHAPE as geometry), ST_XMIN(SHAPE) as minx, ST_YMIN(SHAPE) as miny, ST_XMAX(SHAPE) as maxx, ST_YMAX(SHAPE) as maxy FROM wdpa ORDER BY ST_Hilbert(SHAPE, ST_Extent(ST_MakeEnvelope(-180, -90, 180, 90)))) TO '/data/wdpa.parquet' (FORMAT PARQUET, COMPRESSION zstd);
