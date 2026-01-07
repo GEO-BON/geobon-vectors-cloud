74480004a887e6f7bc358841aecd0cf86bc11399
Guillaume Larocque <glaroc@gmail.com>
2026-01-07 17:26:02 -0500

    Fix WDPA through small row groups

--- datasets/wdpa.sh
+++ datasets/wdpa.sh
@@ -4,12 +4,13 @@
 # Convert the GDB to Parquet format using ogr2ogr
 #wget -P /data/ https://d1gam3xoknrgr2.cloudfront.net/current/WDPA_Dec2025_Public.zip
 #unzip /data/WDPA_Dec2025_Public.zip -d /data/wdpa_dec2025_public   
-ogr2ogr /data/wdpa.gpkg /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb WDPA_Dec2025_Public -lco GEOMETRY=AS_WKB -nlt PROMOTE_TO_MULTI -nln wdpa
-#/root/.duckdb/cli/latest/duckdb < /datasets/wdpa.sql
-gpio convert /data/wdpa.gpkg /data/tmp.parquet
-gpio check all /data/tmp.parquet --fix --fix-output wdpa.parquet
+ogr2ogr /data/wdpa.fgb /data/wdpa_dec2025_public/WDPA_Dec2025_Public.gdb WDPA_poly_Dec2025 -nlt PROMOTE_TO_MULTI -nln wdpa -simplify 0.00005 -makevalid
+/root/.duckdb/cli/latest/duckdb < /datasets/wdpa.sql
+#gpio convert /data/wdpa.fgb /data/wdpa.parquet
+#gpio convert /data/wdpa_duck.parquet /data/wdpa.parquet
+#gpio check all /data/tmp.parquet --fix --fix-output wdpa.parquet
 
-s5cmd cp --acl "public-read" /data/wdpa.parquet s3://bq-io/vectors-cloud/wdpa/
+s5cmd cp --acl "public-read" /data/wdpa_duck.parquet s3://bq-io/vectors-cloud/wdpa/wdpa.parquet
 
 # docker compose run --rm gdal /datasets/wdpa.sh
 
